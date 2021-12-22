77d71e565f5f69180c523ef689cfa6e883cd2329
dmargu <danielmargulies@rocketmail.com>
2021-12-22 16:02:53 -0500

    added src folder and moved files around... apps are working

--- mobileapp/App.tsx
+++ mobileapp/App.tsx
@@ -1,9 +1,10 @@
+//leaving this file out of src folder to prevent problems with node modules
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import Navigation from './src/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
